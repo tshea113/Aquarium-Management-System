bfaf4bb9b48bdba13b6528ef01f7179ab9372a2f
tshea113 <Tylershea12@gmail.com>
2019-08-25 00:39:38 -0400

    Added base home page design

---
+++ website/client/src/components/AppNavigation.vue
@@ -0,0 +1,42 @@
+<template>
+    <span>
+        <v-navigation-drawer app v-model="drawer" class="indigo darken-4" dark disable-resize-watcher>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index">
+                        <v-list-tile-content>
+                            {{item.title}}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar app color="gray darken-4" dark>
+            <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-btn flat class="hidden-sm-and-down mx-2">LOGIN</v-btn>
+            <v-btn color="deep-orange darken-3" class="hidden-sm-and-down mx-2">SIGN UP</v-btn>
+        </v-app-bar>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Meal Prep',
+            drawer: false,
+            items: [
+                { title: 'Menu' },
+                { title: 'Sign In' },
+                { title: 'Join' }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ website/client/src/components/Welcome.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container fluid fill-height class="welcome" style="max-height: 100vh;">
+        <v-layout justify-center align-center column pa-5>
+            <div id="content" class="display-3 font-weight-black white--text text-xs-center mb-3">AQUARIUM MANAGEMENT SYSTEM</div>
+            <div id="content" class="display-1 font-weight-bold white--text text-xs-center">The all-in-one solution for remote aquarium management.</div>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Welcome'
+};
+</script>
+
+<style scoped>
+.welcome {
+    background: url('../assets/koi-background.jpg');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+#content {
+    text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+</style>

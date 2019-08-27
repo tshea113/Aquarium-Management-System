a5da7151e1d8bb44cdd3e1dc6b238536df354c28
tshea113 <Tylershea12@gmail.com>
2019-08-27 15:58:16 -0400

    Move flask to vue frontend

--- website/backend/main.py
+++ website/backend/main.py
@@ -8,13 +8,4 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    # Return a personalized home page for a logged in user
-    if not current_user.is_authenticated:
-        return render_template('index.html')
-    else:
-        return render_template('login_index.html', name=current_user.email)
-
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('login_index.html', name=current_user.email)
+    return render_template('index.html')

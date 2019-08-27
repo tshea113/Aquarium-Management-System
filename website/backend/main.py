e02d3e149781d8490962f411c9ecadda9ecffb23
tshea113 <Tylershea12@gmail.com>
2019-08-27 16:14:30 -0400

    Added flask configuration for vue

--- website/backend/main.py
+++ website/backend/main.py
@@ -1,11 +1,15 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from flask_login import login_required, current_user
 from . import db
+from jinja2 import TemplateNotFound
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
